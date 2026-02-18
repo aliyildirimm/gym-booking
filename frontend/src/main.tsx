3c7520070e47b37425513a7ffc6f7ef6d180a16a
Ali Yıldırım <aliyildirim@sabanciuniv.edu>
2026-02-18 14:28:44 +0100

    delete dones

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
 import App from './App'
+
+defineCustomElements(window)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
